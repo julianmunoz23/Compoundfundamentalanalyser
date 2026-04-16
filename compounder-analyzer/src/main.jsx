ab7e547da0999135da4719a6c27290b9e4d3327c
julianmunoz23 <julian.munoz@confeccionesparches.com>
2026-04-15 19:51:41 -0500

    Update main.jsx

--- compounder-analyzer/src/main.jsx
+++ compounder-analyzer/src/main.jsx
@@ -1,5 +1,12 @@
-import { inject } from '@vercel/analytics';
-inject();
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { inject } from '@vercel/analytics'
+
+inject()
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
