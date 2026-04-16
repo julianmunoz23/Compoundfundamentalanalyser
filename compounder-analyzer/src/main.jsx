80cf9cb056f14f53822d1be4eb089865300916ad
julianmunoz23 <julian.munoz@confeccionesparches.com>
2026-04-15 19:46:53 -0500

    Update main.jsx

--- compounder-analyzer/src/main.jsx
+++ compounder-analyzer/src/main.jsx
@@ -1,7 +1,5 @@
-Disculpas es este el actual
 import { inject } from '@vercel/analytics';
 inject();
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-...
